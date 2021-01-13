c1b7ae0c9ee5f87057923f6db104701acd899f96
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-01-14 02:24:51 +0600

    mode stats

---
+++ StatasticsUsingPython/02Mode.py
@@ -0,0 +1,12 @@
+# import the necessary packages
+import statistics
+# mode identifies the number that was used the most
+
+dataset = [1,2,4,8,1,9,12,4,6,7,4,9,4]
+
+mode = statistics.mode(dataset)
+
+print("mode using stat "+mode)
+
+
+
