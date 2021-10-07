c0f1eff44917c69b754f13b27909ab1ce2acb850
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-10-07 07:46:13 +0600

    Add files via upload

---
+++ NLP/sentiementAnnalysis/main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Open the file
+
+text = open("read.txt", encoding="UTF-8").read()
+print(text)
