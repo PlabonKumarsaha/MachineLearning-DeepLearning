e6dbce3928fa7a019e82a6aa5c607476812b9104
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-10-07 17:34:24 +0600

    Add files via upload

--- NLP/sentiementAnnalysis/main.py
+++ NLP/sentiementAnnalysis/main.py
@@ -1,9 +1,46 @@
-# This is a sample Python script.
+import string
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-# Open the file
+# reading text file
+text = open("read.txt", encoding="utf-8").read()
 
-text = open("read.txt", encoding="UTF-8").read()
-print(text)
+# converting to lowercase
+lower_case = text.lower()
+print('low'+lower_case)
+
+# Removing punctuations from the text
+cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+print('cleaned up'+ cleaned_text)
+
+
+# splitting text into words
+tokenized_words = cleaned_text.split()
+
+# Stop words are words irrelevant to detecting sentiment
+stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
+              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
+              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
+              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
+              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
+              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+# Removing stop words from the tokenized words list
+final_words = []
+for word in tokenized_words:
+    if word not in stop_words:
+        final_words.append(word)
+
+print('Tokenization \n')
+print(final_words)
+
+# Sentiment algo
+# 1. check if the tokenizaed words matches any word with emotions text file
+
+with open('emotions.txt','r')as file:
+    for line in file:
+        print(line)
+
